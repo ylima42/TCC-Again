1c245e4adf21ce03750ad0e8f983ac5fb9a323f9
Yaquene Lima <ylima@c1r5s2.42luanda.com>
2025-05-07 21:01:24 +0100

    corrigi o erro da base de dados e 1h:04

--- connetion.php
+++ connetion.php
@@ -1,8 +1,9 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','projeto_db') or die ('falha na conexao');
+$conn = mysqli_connect('localhost','root','','doces_db') or die ('falha na conexao');
 $host = 'localhost';     // ou 127.0.0.1
 $usuario = 'root';       // teu usuário MySQL
 $senha = '';             // tua senha do MySQL (deixa vazio se não tiver)
 $banco = 'ecommerce'; // nome do banco
+
 ?>
