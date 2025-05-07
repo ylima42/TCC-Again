e50534730590bebb82e623eeff3c22d6f511ed9f
Yaquene Lima <ylima@c1r4s3.42luanda.com>
2025-05-07 14:23:51 +0100

    1h:02

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -8,36 +8,39 @@
   <title>Document</title>
 
 </head>
-  <body>
-    <header class="header">
-      <div class="flex">
-        <a href="page.php" class="img/logo.jpg"></a>
-            <nav class="navbar">
-                <a href="page.php">Inicio</a>       
-                <a href="produt.html" class="nav-link">Atualizar produtos</a>
-                <a href="admin_order.php">Pedidos</a>
-                <a href="admin_user.php">Usuario</a>
-                <a href="admin_message">Menssagens</a>
-                <a href="add_item.html" class="nav-link">Adicionar produto</a>
-            </nav>
-            <div class="icons">
-                <i class="bi bi-person" id="user-btn"></i>
-                <i class="bi bi-list" id="menu-btn"></i>
-            </div>
-            <div class="user-box">
-                <p>Username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
-                <p>Email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
-                <form method="post">
-                    <button type="submit" class="logout-btn">Sair</button>
-                </form>
-            </div>
-        </div>     
-    </header>
-        <div class="bnner">
-            < class="detail">
-                <h1>Dashboard do administrador</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-</div>
-        </div>
-  </body>
+<body>
+  <header class="header">
+    <div class="flex">
+      <a href="page.php"><img src="img/logo.jpg" alt="Logo"></a>
+      <nav class="navbar">
+        <a href="page.php">Início</a>       
+        <a href="produt.html" class="nav-link">Atualizar produtos</a>
+        <a href="admin_order.php">Pedidos</a>
+        <a href="admin_user.php">Usuário</a>
+        <a href="admin_message.php">Mensagens</a>
+        <a href="add_item.html" class="nav-link">Adicionar produto</a>
+      </nav>
+      <div class="icons">
+        <i class="bi bi-person" id="user-btn"></i>
+        <i class="bi bi-list" id="menu-btn"></i>
+      </div>
+      <div class="user-box">
+        <p>Username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
+        <p>Email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
+        <form method="post">
+          <button type="submit" class="logout-btn">Sair</button>
+        </form>
+      </div>
+    </div>     
+  </header>
+
+  <div class="banner">
+    <div class="detail">
+      <h1>Dashboard do administrador</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+  </div>
+    <div class="line"></div>
+</body>
+
 </html>
